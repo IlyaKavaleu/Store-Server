e0365b5eb4105dece130f89e56ad2c32cb515149
ilia <kavaleuilia@gmail.com>
2024-02-14 01:10:04 +0100

    final api

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
-from api.views import ProductModelViewSet
+from api.views import ProductModelViewSet, BasketModelViewSet
 from rest_framework import routers
 
 app_name = 'api'
 
 router = routers.DefaultRouter()
 router.register(r'products', ProductModelViewSet)
+router.register(r'baskets', BasketModelViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
--- products/serializers.py
+++ products/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from products.models import Product, ProductCategory
+from products.models import Product, ProductCategory, Basket
+from rest_framework import fields
 
 
 class ProductSerializer(serializers.ModelSerializer):
@@ -8,3 +9,21 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ('id', 'name', 'description', 'price', 'quantity', 'image', 'category')
+
+
+class BasketSerializer(serializers.ModelSerializer):
+    product = ProductSerializer()
+    sum = fields.FloatField(required=False)
+    price_all_products = fields.SerializerMethodField()
+    sum_all_products = fields.SerializerMethodField()
+
+    class Meta:
+        model = Basket
+        fields = ('id', 'product', 'quantity', 'sum', 'price_all_products', 'sum_all_products', 'created_timestamp')
+        read_only_fields = ('created_timestamp',)
+
+    def get_price_all_products(self, obj):
+        return Basket.objects.filter(user_id=obj.user.id).price_all_products()
+
+    def get_sum_all_products(self, obj):
+        return Basket.objects.filter(user_id=obj.user.id).sum_all_products()
